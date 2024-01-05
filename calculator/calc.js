20c23c8f04c82eeb9f24424596c008b4a2e282aa
Ketchell T <Ketchell.terzakis@vtsd.com>
2024-01-05 11:09:58 -0500

    No

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,27 +1,27 @@
 //setup access to html elements
 const button = document.querySelectorAll('button');
-const display = document.querySelector('display');
+const display = document.querySelector('.display');
 //add listeners to all buttons
-buttons.forEach(
+button.forEach(
   function(button){
-      button.addEventListener('click', calculate);
+      button.addEventListener("click", calculate);
 
   }
-);
+)
 //calculate function
 function calculate(event) {
     //current clicked buttons value
     const clickedValue = event.target.value;
 //check if = sign was clicked
-    if (clickedValue === '=') {
+    if (clickedValue === "=") {
         //check if display is empty
+        if (display.value !== "") {
+            display.value = eval(display.value);
     }
-    if (display.value !== '') {
-        display.value = eval(display.value);
 
-    } else if (clickedValue === 'C') {
+    } else if (clickedValue === "C") {
         //clear display
-        display.value = '';
+        display.value = "";
     } else {
         //otherwise concatenate it to the display
         display.value += clickedValue;
